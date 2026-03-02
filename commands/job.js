795cc3a32fe6325c1fb74ff192401e31685081ac
LegendenXX <91482791+LegendenXX@users.noreply.github.com>
2026-03-02 11:50:18 +0100

    job und Market zusammen mit config überarbeitet

---
+++ commands/job.js
@@ -0,0 +1,20 @@
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+const fs = require('fs');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('jobs')
+        .setDescription('Übersicht aller Berufe'),
+    async execute(interaction) {
+        const jobs = JSON.parse(fs.readFileSync('./config_jobdb.json', 'utf8'));
+        const embed = new EmbedBuilder()
+            .setTitle('💼 Job-Zentrum')
+            .setColor(0x3498db);
+
+        for (const [id, data] of Object.entries(jobs)) {
+            embed.addFields({ name: data.name, value: `Lohn: ${data.lohn}€ | ID: \`${id}\`` });
+        }
+
+        await interaction.reply({ embeds: [embed] });
+    }
+};
