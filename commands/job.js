8c32b7a5a65a35fdaac493ce824f9ef500954b5a
LegendenXX <91482791+LegendenXX@users.noreply.github.com>
2026-03-02 13:58:28 +0100

    "

--- commands/job.js
+++ commands/job.js
@@ -1,47 +1,30 @@
 // commands/jobs.js
-const { SlashCommandBuilder, EmbedBuilder, ActionRowBuilder, StringSelectMenuBuilder, MessageFlags } = require('discord.js');
-const jobs = require('../helpers/jobsConfig.js');
+const { SlashCommandBuilder, ActionRowBuilder, StringSelectMenuBuilder, MessageFlags } = require('discord.js');
+const jobs = require('../jobsConfig.js');
 
 module.exports = {
     data: new SlashCommandBuilder()
-        .setName('jobs')
-        .setDescription('Zeige alle verfügbaren Jobs'),
+        .setName('Jobs')
+        .setDescription('alle verfügbaren Jobs'),
 
     async execute(interaction) {
-        try {
-            const menu = new StringSelectMenuBuilder()
-                .setCustomId('select-job')
-                .setPlaceholder('Wähle einen Job aus...')
-                .addOptions(
-                    Object.keys(jobs).map(jobKey => ({
-                        label: jobs[jobKey].name,
-                        value: jobKey,
-                        description: `Lohn: ${jobs[jobKey].lohn}€ | Typ: ${jobs[jobKey].type}`
-                    }))
-                );
+        const menu = new StringSelectMenuBuilder()
+            .setCustomId('select-job')
+            .setPlaceholder('Wähle einen Job aus...')
+            .addOptions(
+                Object.keys(jobs).map(jobKey => ({
+                    label: jobs[jobKey].name,
+                    value: jobKey,
+                    description: `Lohn: ${jobs[jobKey].lohn}€ | Typ: ${jobs[jobKey].type}`
+                }))
+            );
 
-            const row = new ActionRowBuilder().addComponents(menu);
+        const row = new ActionRowBuilder().addComponents(menu);
 
-            await interaction.reply({
-                content: 'Bitte wähle einen Job aus:',
-                components: [row],
-                flags: MessageFlags.Ephemeral
-            });
-
-        } catch (err) {
-            console.error('Jobs Command Fehler:', err);
-
-            if (!interaction.replied && !interaction.deferred) {
-                await interaction.reply({
-                    content: '❌ Fehler beim Laden der Jobs.',
-                    flags: MessageFlags.Ephemeral
-                });
-            } else {
-                await interaction.followUp({
-                    content: '❌ Fehler beim Laden der Jobs.',
-                    flags: MessageFlags.Ephemeral
-                });
-            }
-        }
+        await interaction.reply({
+            content: 'Bitte wähle einen Job aus:',
+            components: [row],
+            flags: MessageFlags.Ephemeral 
+        });
     }
 };
